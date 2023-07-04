d85bc5cc6b683fbeaf70a6be5d452de560cc5caa
Di3go-Opr3a <di3go.opr3a@gmail.com>
2023-07-04 09:47:07 +0200

    update exercise

--- test.py
+++ test.py
@@ -1,25 +1,23 @@
-matrix = [["I", "V", "X", "L", "C", "D", "M"], [1, 5, 10, 50, 100, 500, 1000]]
+roman_number = { "I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
 s = "DCXXI"
 sum = 0
 x = 0
 
 while x < len(s):
-    symbol = s[x]
     if x < (len(s)-1):
-        symbol2 = s[x+1]
-        if (symbol == "I" and symbol2 == "V") or (symbol == "I" and symbol2 == "X"):
-            sum += (matrix[1][matrix[0].index(symbol2)] - matrix[1][matrix[0].index(symbol)])
+        if (s[x] == "I" and (s[x+1] == "V" or s[x+1] == "X")):
+            sum += (roman_number[s[x+1]] - roman_number[s[x]])
             x += 2
-        elif (symbol == "X" and symbol2 == "L") or (symbol == "X" and symbol2 == "C"):
-            sum += (matrix[1][matrix[0].index(symbol2)] - matrix[1][matrix[0].index(symbol)])
+        elif (s[x] == "X" and (s[x+1] == "L" or  s[x+1] == "C")):
+            sum += (roman_number[s[x+1]] - roman_number[s[x]])
             x += 2
-        elif (symbol == "C" and symbol2 == "D") or (symbol == "C" and symbol2 == "M"):
-            sum += (matrix[1][matrix[0].index(symbol2)] - matrix[1][matrix[0].index(symbol)])
+        elif (s[x] == "C" and (s[x+1] == "D" or s[x+1] == "M")):
+            sum += (roman_number[s[x+1]] - roman_number[s[x]])
             x += 2
         else:
-            sum += matrix[1][matrix[0].index(symbol)]
+            sum += roman_number[s[x]]
             x += 1
     else:
-        sum += matrix[1][matrix[0].index(symbol)]
+        sum += roman_number[s[x]]
         x += 1
 print(sum)
